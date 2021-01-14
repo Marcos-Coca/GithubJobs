40351956fe279b1959fdc5694a60a919e0bc86de
Marcos Mercado <marcoscoca0905@gmail.com>
2021-01-13 21:24:41 -0400

    Configuration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    historyApiFallback: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  output: {
+    filename: '[name].bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html')
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: './src/**/*'
+      }
+    })
+  ]
+}
